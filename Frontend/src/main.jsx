58a903d4edd88a8bfc5272e0b0503f21dcddfc38
NK8998 <ne.kioko16@gmail.com>
2025-10-29 10:56:29 +0300

    mine

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./AuthContext.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ClerkProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
