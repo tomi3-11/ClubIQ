3dd78bfb47a56c58db7d82d65f924e09868e5a0a
Thomas Wotoro <didymusthomas6@gmail.com>
2025-12-20 22:33:43 +0300

    Removed and commented the CSRF implemetation for quick testing purposes

--- Backend/app/forms.py
+++ Backend/app/forms.py
@@ -1,18 +1,18 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField, TextAreaField, DateTimeField, PasswordField
-# from wtforms.validators import Email, DataRequired, Length, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, DateTimeField, PasswordField
+from wtforms.validators import Email, DataRequired, Length, EqualTo
 # from app import csrf
 
-# class MemberForm(FlaskForm):
-#     role = StringField('Role', validators=[DataRequired(), Length(min=2, max=64)])
-#     submit = SubmitField('Submit')
+class MemberForm(FlaskForm):
+    role = StringField('Role', validators=[DataRequired(), Length(min=2, max=64)])
+    submit = SubmitField('Submit')
     
 
-# class RegistrationForm(FlaskForm):
-#     class Meta:
-#         csrf = False
+class RegistrationForm(FlaskForm):
+    # class Meta:
+    #     csrf = False
         
-#     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
-#     email = StringField('Email', validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
-#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
